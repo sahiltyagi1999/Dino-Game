01ce35108b698c6d84fd56f83e9c77a8979c3c50
Sahil Tyagi <136681974+sahiltyagi1999@users.noreply.github.com>
2023-07-24 15:25:00 +0530

    script.js
    Added Score after each jump, so that player can succesfully she their score after finishing the game

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-const dino=document.getElementById("dino")
-const cactus=document.getElementById("cactus")
-function jump(){
-    if(dispatchEvent.classList!="jump"){
-        dino.classList.add("jump");
-        setTimeout(function(){
-            dino.classList.remove("jump");
-        },300)
-    }
-}
+const dino = document.getElementById("dino");
+        const cactus = document.getElementById("cactus");
+        let score = 0;
 
-let checkAlive = setInterval(function(){
-    let dinoTop= parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-    let cactusLeft= parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
-    if(cactusLeft>0&& cactusLeft<70 && dinoTop>=143){
-        dino.style.animationPlayState="paused";
-        cactus.style.animationPlayState="paused";
-        alert("Game Over  ");
-        window.location.reload();
-    }
-},10);
-document.addEventListener("keydown",function(event){
-    jump();
+        function jump() {
+            if (dino.classList != "jump") {
+                dino.classList.add("jump");
+                setTimeout(function () {
+                    dino.classList.remove("jump");
+                }, 300);
+            }
+        }
+
+        let checkAlive = setInterval(function () {
+            let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+            let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+            if (cactusLeft > 0 && cactusLeft < 70 && dinoTop >= 143) {
+                dino.style.animationPlayState = "paused";
+                cactus.style.animationPlayState = "paused";
+                alert("Game Over. Your Score: " + score);
+                window.location.reload();
+            } else if (cactusLeft < 0) {
+                // Dino successfully jumped over the cactus
+                score++;
+            }
+        }, 10);
+
+        document.addEventListener("keydown", function (event) {
+            jump();
 })
